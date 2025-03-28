99b2c18cac55b6df9e572ea69ebb9cab9a3edcf2
Kunal Verma <vkunal591@gmail.com>
2025-03-29 01:33:29 +0530

    remove unwanted code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,10 +43,6 @@ export default function Home() {
       <DedicatedPartner />
       <InsightClients />
       <GetInTouch />
-      <div className="h-screen">
-        .
-
-      </div>
     </div>
   );
 }
